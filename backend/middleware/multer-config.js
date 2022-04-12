d2e86a2b0563726903923440e86232318accb004
NASRINE-H <64153322+NASRINE-H@users.noreply.github.com>
2022-04-12 12:17:00 +0200

    modification exports.likeDislike

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,27 +1,26 @@
 // On importe multer qui est un package qui permet de gérer les fichiers entrants dans les requêtes HTTP
 const multer = require('multer');
-// On crée un dictionnaire des types MIME pour définire le format des images
-// Donc la creation d'un objet pour ajouter une extention en fonction du type mime du ficher
+
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
-// On crée un objet de configuration pour préciser à multer où enregistrer les fichiers images et les renommer
+
 const storage = multer.diskStorage({
-    // On mets la destination d'enregistrement des images
+   
   destination: (req, file, callback) => {
-      // On passe le dossier images qu'on a créé dans le backend
+      
     callback(null, 'images');
   },
-  // On dit à multer quel nom de fichier on utilise pour éviter les doublons
+
   filename: (req, file, callback) => {
-      // On génère un nouveau nom avec le nom d'origine, on supprime les espaces (white space avec split) et on insère des underscores à la place
+     
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     
     callback(null, name + Date.now() + '.' + extension);
   }
 });
-// On export le module, on lui passe l'objet storage, la méthode single pour dire que c'est un fichier unique et on précise que c'est une image
+
 module.exports = multer({storage: storage}).single('image');
